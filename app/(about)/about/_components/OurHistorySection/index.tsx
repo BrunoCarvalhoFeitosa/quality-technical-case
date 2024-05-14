e71419fca1bfc66a7eb601dfd655126960378b2c
Bruno Carvalho Feitosa <brunocarvalhofeitosa@outlook.com>
2024-05-14 00:19:43 -0300

    fix: about page phrases

--- app/(about)/about/_components/OurHistorySection/index.tsx
+++ app/(about)/about/_components/OurHistorySection/index.tsx
@@ -2,21 +2,18 @@
 import * as Styled from "./styled";
 
 export const OurHistorySection = () => {
-    return (
-        <Styled.OurHistoryContainer>
-            <Styled.OurHistoryWrapper>
-                <Styled.OurHistoryNumber>
-                    01
-                </Styled.OurHistoryNumber>
-                <Styled.OurHistoryTitle>
-                    Começamos nossa <strong>história</strong> em 2016 em um 
-                    pequeno escritório na cidade de São Paulo, dois
-                    estudantes de engenharia da computação decidiram 
-                    oferecer serviços digitais para os clientes próximos 
-                    ao escritório. Home nossa equipe é robusta, mais de 
-                    100 funcionários trabalham em diversos produtos.
-                </Styled.OurHistoryTitle>
-            </Styled.OurHistoryWrapper>
-        </Styled.OurHistoryContainer>
-    );
+  return (
+    <Styled.OurHistoryContainer>
+      <Styled.OurHistoryWrapper>
+        <Styled.OurHistoryNumber>01</Styled.OurHistoryNumber>
+        <Styled.OurHistoryTitle>
+          Começamos nossa <strong>história</strong> em 2016 em um pequeno
+          escritório na cidade de São Paulo, dois estudantes de engenharia da
+          computação decidiram oferecer serviços digitais para os clientes
+          próximos ao escritório. Hoje nossa equipe é robusta, mais de 100
+          funcionários trabalham em diversos de nossos produtos.
+        </Styled.OurHistoryTitle>
+      </Styled.OurHistoryWrapper>
+    </Styled.OurHistoryContainer>
+  );
 };
--- app/(about)/about/_components/OurValuesSection/index.tsx
+++ app/(about)/about/_components/OurValuesSection/index.tsx
@@ -2,21 +2,17 @@
 import * as Styled from "./styled";
 
 export const OurValuesSection = () => {
-    return (
-        <Styled.OurValuesContainer>
-            <Styled.OurValuesWrapper>
-                <Styled.OurValuesNumber>
-                    03
-                </Styled.OurValuesNumber>
-                <Styled.OurValuesTitle>
-                    Nossa <strong>visão</strong> é atingir
-                    qualquer público alvo com os nossos serviços,
-                    queremos revolucionar o meio digital para que qualquer
-                    pessoa, independente da idade acesse a internet e preferencialmente
-                    as nossas ferramentas.
-
-                </Styled.OurValuesTitle>
-            </Styled.OurValuesWrapper>
-        </Styled.OurValuesContainer>
-    );
+  return (
+    <Styled.OurValuesContainer>
+      <Styled.OurValuesWrapper>
+        <Styled.OurValuesNumber>03</Styled.OurValuesNumber>
+        <Styled.OurValuesTitle>
+          Nossa <strong>visão</strong> é atingir qualquer público alvo com os
+          nossos serviços, queremos revolucionar o meio digital para que
+          qualquer pessoa, independente da idade acesse a internet e
+          preferencialmente os nossos serviços e as nossas ferramentas.
+        </Styled.OurValuesTitle>
+      </Styled.OurValuesWrapper>
+    </Styled.OurValuesContainer>
+  );
 };
